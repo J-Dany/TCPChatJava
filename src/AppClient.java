20ea6c55755873864b2df95d5c00e99e4d4fffdf
J-Dany <jdanya@outlook.com>
2020-12-20 00:28:39 +0100

    GUI funzionante, non molto bella ma dettagli. Bisogna implementare altre funzionalità sulla GUI, come ad esempio quando si preme invio viene mandato il messaggio

--- src/AppClient.java
+++ src/AppClient.java
@@ -2,12 +2,17 @@ package src;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 import javax.swing.plaf.FontUIResource;
 import javax.swing.plaf.InsetsUIResource;
+import javax.swing.text.StyledDocument;
 
 import java.net.Socket;
 import java.io.OutputStreamWriter;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ConnectException;
@@ -16,8 +21,7 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
-public class AppClient 
-{
+public class AppClient {
     /**
      * Codici di ritorno dell'applicazione
      */
@@ -28,189 +32,84 @@ public class AppClient
     /**
      * Grandezza dell'applicazione
      */
-    public static final int HEIGHT = 600;
-    public static final int WIDTH  = 900;
+    public static final int HEIGHT = 480;
+    public static final int WIDTH = 853;
 
     public static void main(String[] args) 
     {
-        /*if (args.length == 0) 
-        {
-            System.out.println("Non sono stati forniti argomenti all'applicazione");
-            System.exit(1);
-        }
-
-        Socket socket = new Socket();
-        InetSocketAddress server_address = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
         try 
         {
-            socket.connect(server_address);
-        } 
-        catch (ConnectException e)
-        {
-            e.printStackTrace();
-            System.exit(CONNESSIONE_RIFIUTATA);
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-            System.exit(IO_EXCEPTION);
-        }*/
-
-        try
-        {
-            /*Socket socket = new Socket();
+            Socket socket = new Socket();
             InetSocketAddress server_address = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
-            socket.connect(server_address);*/
+            socket.connect(server_address);
 
-            ChatUI chat = new ChatUI(null);
+            Scanner input = new Scanner(System.in);
+            System.out.print("Nome utente: ");
+            String nome = input.nextLine();
+            socket.getOutputStream().write((nome + "%%!").getBytes());
+            input.close();
+
+            ChatUI chat = new ChatUI(socket, new String(nome));
             chat.prepareApp();
             chat.show();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
 
-        /*String nomeUtente;
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Inserisci nome utente: ");
-        nomeUtente = input.nextLine();
-
-        OutputStreamWriter write = null;
-        try 
-        {
-            write = new OutputStreamWriter(socket.getOutputStream(), "ISO-8859-1");
-
-            write.write(nomeUtente + "%%!");
-            write.flush();
-        } 
-        catch (Exception e) 
-        {
-            e.printStackTrace();
-        }
-
-        if (write == null) 
-        {
-            System.exit(OUTPUT_STREAM_NON_ISTANZIATO);
-        }
-
-        System.out.println("Il messaggio deve essere lungo max. 256 e, per smettere di inviare i messaggi, inserisci '!close'");
-        System.out.println("*********************************");
-
-        Thread read = new Thread(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                while (!Thread.currentThread().isInterrupted())
-                {
-                    try
+            Thread read = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (!Thread.currentThread().isInterrupted()) 
                     {
-                        byte[] buffer = new byte[1024];
-                        int l = socket.getInputStream().read(buffer);
-                        String msg = new String(buffer, 0, l, "ISO-8859-1");
-
-                        if (msg.equals("UTENTE_NON_RICONOSCIUTO"))
+                        try 
                         {
+                            byte[] buffer = new byte[1024];
+                            int l = socket.getInputStream().read(buffer);
+                            String msg = new String(buffer, 0, l, "ISO-8859-1");
+
+                            if (msg.equals("UTENTE_NON_RICONOSCIUTO")) 
+                            {
+                                break;
+                            }
+
+                            chat.aggiungiMessaggio(msg);
+                        } 
+                        catch (Exception e) 
+                        {
+                            e.printStackTrace();
                             break;
                         }
-
-                        System.out.println("\n" + msg);
-                    }
-                    catch (Exception e)
-                    {
-                        e.printStackTrace();
                     }
                 }
-            }
-        });
-        read.start();
-
-        String msg = "";
-        while (msg != null) 
-        {
-            if (socket.isClosed())
-            {
-                break;
-            }
-
-            System.out.print("Tu: ");
-            msg = input.nextLine();
-
-            if (msg.equals("!close")) 
-            {
-                msg = "close";
-                
-                try 
-                {
-                    write.write(msg);
-                    write.flush();
-                } 
-                catch (IOException e) 
-                {
-                    e.printStackTrace();
-                }
-
-                break;
-            }
-            else
-            {
-                try
-                {
-                    String data = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                    String time = LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss"));
-                    
-                    // Struttura messaggio: Data Tempo|Utente|Messaggio
-                    write.write(data + " " + time + "|" + nomeUtente + "|" + msg);
-                    write.flush();
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        read.interrupt();
-
-        try
-        {
-            socket.close();
-        }
-        catch (Exception e)
-        {
+            });
+            read.start();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        
-        input.close();*/
     }
 
-    static class ChatUI
-    {
+    static class ChatUI {
         private OutputStreamWriter writer;
         private Socket socket;
         private JFrame app;
+        private JTextField input;
         private Font font = new FontUIResource("Noto Sans", Font.PLAIN, 14);
-        private Font fontTextArea = new FontUIResource("Caladea", Font.PLAIN, 16);
+        private Font fontTextArea = new FontUIResource("Caladea", Font.PLAIN, 18);
         private Font fontNomeUtente = new FontUIResource("Noto Sans", Font.PLAIN, 18);
         private JTextPane textArea;
+        private StyledDocument doc;
+        private String nome;
 
-        public ChatUI(Socket socket) throws IOException
+        public ChatUI(Socket socket, String nome) throws IOException 
         {
+            this.nome = nome;
             this.app = new JFrame("Chat");
-            if (socket != null)
-            {
+            if (socket != null) {
                 this.socket = socket;
                 this.writer = new OutputStreamWriter(this.socket.getOutputStream(), "ISO-8859-1");
             }
         }
 
-        public void prepareApp()
+        public void prepareApp() 
         {
             this.app.setSize(WIDTH, HEIGHT);
-            this.app.setLayout(new GridLayout(2, 1));
             this.app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
             JMenuBar menuBar = new JMenuBar();
@@ -226,52 +125,119 @@ public class AppClient
             menuBar.add(file);
             menuBar.add(info);
             info.add(about);
+            this.app.setJMenuBar(menuBar);
 
-            JPanel panel1 = new JPanel();
-            panel1.setLayout(new GridLayout(1, 2));
+            JPanel panel = new JPanel();
+            BoxLayout layout = new BoxLayout(panel, BoxLayout.PAGE_AXIS);
+            panel.setLayout(layout);
+            this.app.setContentPane(panel);
 
             this.textArea = new JTextPane();
             this.textArea.setFont(fontTextArea);
+            this.textArea.setPreferredSize(new Dimension(WIDTH, 400));
             this.textArea.setEditable(false);
-            panel1.add(this.textArea);
+            panel.add(this.textArea);
 
-            JPanel containerNomeUtente = new JPanel();
-            JTextField nomeUtente = new JTextField();
-            nomeUtente.setPreferredSize(new Dimension(WIDTH / 2, 64));
-            nomeUtente.setMargin(new InsetsUIResource(20, 20, 20, 20));
-            nomeUtente.setFont(fontNomeUtente);
-            JButton buttonInviaNomeUtente = new JButton("Login");
-            buttonInviaNomeUtente.setFont(font);
-            containerNomeUtente.add(nomeUtente);
-            containerNomeUtente.add(buttonInviaNomeUtente);
-            panel1.add(containerNomeUtente);
+            JPanel panelInput = new JPanel();
+            panelInput.setBackground(Color.WHITE);
+            BoxLayout layoutInput = new BoxLayout(panelInput, BoxLayout.LINE_AXIS);
+            panelInput.setLayout(layoutInput);
 
-            JPanel panel = new JPanel();
-            panel.setLayout(new GridLayout(1, 2));
-            panel.setBackground(Color.BLACK);
+            input = new JTextField();
+            input.setFont(fontNomeUtente);
+            panelInput.add(input);
 
-            JTextField input = new JTextField();
-            input.setFont(font);
-            panel.add(input);
+            JButton buttonInviaMessaggio = new JButton("Invia");
+            buttonInviaMessaggio.setPreferredSize(new Dimension(100, 40));
+            buttonInviaMessaggio.setFont(fontNomeUtente);
+            buttonInviaMessaggio.addActionListener(new Action()
+            {
+				@Override
+                public void actionPerformed(ActionEvent arg0) 
+                {
+                    try
+                    {
+                        String data = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")); 
+                        String time = LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss"));
+                        
+                        String msg = input.getText();
+                        aggiungiMessaggio("Tu: " + msg);
+                        writer.write(data + " " + time + "|" + nome + "|" + msg);
+                        writer.flush();
+                        input.setText("");
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+				}
 
-            JButton buttonInvia = new JButton("Invia");
-            buttonInvia.setFont(font);
-            panel.add(buttonInvia);
+				@Override
+                public void addPropertyChangeListener(PropertyChangeListener arg0) 
+                {
+					// TODO Auto-generated method stub
+					
+				}
 
-            this.app.setJMenuBar(menuBar);
-            this.app.add(panel1);
-            this.app.add(panel);
+				@Override
+                public Object getValue(String arg0) 
+                {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+				@Override
+                public boolean isEnabled() 
+                {
+					// TODO Auto-generated method stub
+					return false;
+				}
+
+				@Override
+                public void putValue(String arg0, Object arg1) 
+                {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+                public void removePropertyChangeListener(PropertyChangeListener arg0) 
+                {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+                public void setEnabled(boolean arg0) 
+                {
+					// TODO Auto-generated method stub
+					
+                }                
+            });
+            buttonInviaMessaggio.setBorder(null);
+
+            panelInput.add(buttonInviaMessaggio);
+
+            this.doc = this.textArea.getStyledDocument();
+
+            panel.add(panelInput);
         }
 
-        public void show()
+        public void show() 
         {
             this.app.setResizable(false);
             this.app.setVisible(true);
         }
 
-        private void aggiungiMessaggio(String msg)
-        {
-
+        public void aggiungiMessaggio(String msg) {
+            try 
+            {
+                doc.insertString(doc.getLength(), msg + "\n", null);
+            } 
+            catch (Exception e) 
+            {
+                e.printStackTrace();
+            }
         }
     }
 }
