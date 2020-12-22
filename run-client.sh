c041a567e4a652abfbb05d4fe3551feb634cb1fb
J-Dany <jdanya@outlook.com>
2020-12-22 13:49:17 +0100

    Messi i .jar nella posizione giusta per farli usare sia da client che da server

--- run-client.sh
+++ run-client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd out
-java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true src.AppClient $1 $2
+java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -classpath .:./lib/json-20201115.jar src.AppClient $1 $2
