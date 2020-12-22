ea846f2eb1c0415209e8dd7ff3e7a9c107c0b6c4
J-Dany <jdanya@outlook.com>
2020-12-22 15:35:16 +0100

    Appena qualcuno si connette viene aggiornato il numero degli utenti connessi nel client

--- src/Server.java
+++ src/Server.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
 import java.util.Stack;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
@@ -248,6 +251,12 @@ public class Server extends Thread
                 }
 
                 this.logger.add_msg("[ OK  ] - Sto in ascolto per i messaggi di questo client.");
+
+                JSONObject numeroUtenti = new JSONObject();
+                numeroUtenti.put("Tipo-Richiesta", "Numero-Utenti");
+                numeroUtenti.put("Numero", this.connected_clients.size());
+
+                this.messaggioBroadcast(numeroUtenti.toString());
 
                 // Buffer per il messaggio ricevuto
                 this.threadPoolClient.submit(new ConnectionClient(client_socket, c));
