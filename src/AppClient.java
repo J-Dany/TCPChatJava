9cd55a7bda1d3f0ad1ee88343ad15f82f52dbb2e
J-Dany <jdanya@outlook.com>
2020-12-22 19:31:55 +0100

    Adesso non si vede più ogni risposta

--- src/AppClient.java
+++ src/AppClient.java
@@ -98,7 +98,6 @@ public class AppClient
                             }
 
                             JSONObject risposta = new JSONObject(msg);
-                            System.out.println(risposta);
 
                             switch (risposta.getString("Tipo-Richiesta"))
                             {
