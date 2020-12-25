456e61ef80af7acfb343a7940e43aed8d29e80ed
Daniele Castiglia <jdanya@outlook.com>
2020-12-25 12:53:01 +0100

    Refactoring

--- src/Server.java
+++ src/Server.java
@@ -10,9 +10,7 @@ import java.util.Scanner;
 import java.util.Stack;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
 import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
