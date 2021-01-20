437bf9ec3b723ad68cf637c59fbd20ec933ac5c7
deno-c <59620115+deno-c@users.noreply.github.com>
2021-01-20 22:50:21 +0100

    Aggiornati i file di push

--- push.bat
+++ push.bat
@@ -1,9 +1,11 @@
 @echo off
+git rm .project
 git rm Server/.classpath
 git rm Server/.project
 git rm Client/.classpath
 git rm Client/.project
 git add .
+git rm .project
 git rm Server/.classpath
 git rm Server/.project
 git rm Client/.classpath
--- push.sh
+++ push.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+git rm .project
 git rm Server/.classpath
 git rm Server/.project
 git rm Client/.classpath
 git rm Client/.project
 git add .
+git rm .project
 git rm Server/.classpath
 git rm Server/.project
 git rm Client/.classpath
